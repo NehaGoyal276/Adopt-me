9962ba3f0c1a0c251a079bef26d3fa4a5e5c22eb
NehaGoyal276 <goyalneha099@gmail.com>
2022-12-10 15:25:49 -0500

    add-changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
- import { render } from "react-dom";
+import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
       <SearchParams />
     </div>
   );
 };
 
 render(<App />, document.getElementById("root"));
+export default App;
